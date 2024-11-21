9bad6309f74c57751a74f468cdb575dfa1f5b2e9
Aaron Doucett <aaron@doucett.net>
2024-11-21 17:02:14 -0600

    fixes

--- js/about.js
+++ js/about.js
@@ -2,20 +2,20 @@
 // about.js
 
 
-// Initialize Mapbox
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWRvdWNldHQiLCJhIjoiY20zZXZyN20zMGd3MzJycTBxYTFza29iYiJ9.ozrkMII8kTiKtHYTS54P2w';
+//// Initialize Mapbox
+//mapboxgl.accessToken = 'pk.eyJ1IjoiYWRvdWNldHQiLCJhIjoiY20zZXZyN20zMGd3MzJycTBxYTFza29iYiJ9.ozrkMII8kTiKtHYTS54P2w';
 
-const map = new mapboxgl.Map({
-    container: 'about-map',
-    style: 'mapbox://styles/adoucett/cjeg655wt0i482spoj9gsr10l',
-    center: [-74.0060, 40.7128], // Example: New York City coordinates
-    zoom: 5,
-    maxZoom: 12,
-    minZoom: 4
-});
+//const map = new mapboxgl.Map({
+//    container: 'about-map',
+//    style: 'mapbox://styles/adoucett/cjeg655wt0i482spoj9gsr10l',
+//    center: [-74.0060, 40.7128], // Example: New York City coordinates
+//    zoom: 5,
+//    maxZoom: 12,
+//    minZoom: 4
+//});
 
-// Add navigation controls
-map.addControl(new mapboxgl.NavigationControl());
+//// Add navigation controls
+//map.addControl(new mapboxgl.NavigationControl());
 
 
 // Array of words with corresponding CSS classes for styling
